2b250b7eb77b720c4e9edaf4c2df89b388940369
yuras <work.sergeevv@gmail.com>
2021-09-07 10:53:57 +0300

    radio file

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,2 +1,15 @@
-//@prepros-append script.js
-//@prepros-append scroll.js
+window.onload = function (){
+   let buttonsFile = document.querySelectorAll('.btb_file-js')
+    for (const buttonFile of buttonsFile) {
+        buttonFile.addEventListener('click',function (){
+            let file =  this.closest('.input_item').querySelector('.file-js');
+            file.click();
+            file.addEventListener('change', function (){
+                let name = (file.value.split('\\'));
+                console.log(name[name.length-1])
+                buttonFile.innerText = name[name.length-1];
+            })
+        });
+
+    }
+};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+window.onload = function (){
+   let buttonsFile = document.querySelectorAll('.btb_file-js')
+    for (const buttonFile of buttonsFile) {
+        buttonFile.addEventListener('click',function (){
+            let file =  this.closest('.input_item').querySelector('.file-js');
+            file.click();
+            file.addEventListener('change', function (){
+                let name = (file.value.split('\\'));
+                console.log(name[name.length-1])
+                buttonFile.innerText = name[name.length-1];
+            })
+        });
 
-
-
+    }
+};
